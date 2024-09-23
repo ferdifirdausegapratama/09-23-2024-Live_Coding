882d055e19756e35446447a3f87d98ff18740d78
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-09-23 20:47:43 +0700

    Update

--- index.js
+++ index.js
@@ -1,61 +1,7 @@
-var drumButtons = document.querySelectorAll(".drum");
+document.querySelector("button").addEventListener("click", handleClick());
 
-for (let i = 0; i < drumButtons.length; i++) {
-  drumButtons[i].addEventListener("click", function () {
-    let btnInnerHTML = this.innerHTML;
-    makeSound(btnInnerHTML);
-    buttonAnimation(btnInnerHTML);
-  });
+function handleClick(greet){
+  alert("Hello FSW 2"+greet);
 }
 
-//Deteksi keyboard yang ditekan
-document.addEventListener("keypress", function (event) {
-  makeSound(event.key);
-  buttonAnimation(event.key);
-});
-
-function makeSound(key) {
-  switch (key) {
-    case "w":
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
-      break;
-    case "a":
-      var audio = new Audio("sounds/tom-2.mp3");
-      audio.play();
-      break;
-    case "s":
-      var audio = new Audio("sounds/tom-3.mp3");
-      audio.play();
-      break;
-    case "d":
-      var audio = new Audio("sounds/tom-4.mp3");
-      audio.play();
-      break;
-    case "j":
-      var audio = new Audio("sounds/snare.mp3");
-      audio.play();
-      break;
-    case "k":
-      var audio = new Audio("sounds/crash.mp3");
-      audio.play();
-      break;
-    case "l":
-      var audio = new Audio("sounds/kick-bass.mp3");
-      audio.play();
-      break;
-    default:
-      alert("gada button nya");
-  }
-}
-
-function buttonAnimation(key) {
-  let activeBtn = document.querySelector("." + key);
-  activeBtn.classList.add("pressed");
-  activeBtn.classList.add("white");
-
-  setTimeout(function () {
-    activeBtn.classList.remove("pressed");
-    activeBtn.classList.remove("white");
-  }, 100);
-}
+//handleClick;
